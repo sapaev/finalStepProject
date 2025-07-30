84e6c309dd84bc1d42f8a60dae2bd810cf7b899b
qasap <qa,sapaev@gmail.com>
2025-07-31 00:36:08 +0300

    update app

--- app/main.py
+++ app/main.py
@@ -7,7 +7,26 @@ app = Flask(__name__)
 def get_host_ip():
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
-    return f"<strong>Host IP: </strong>{ip}\n"
+    return f"""
+    <html>
+    <head>
+        <style>
+            body {{
+                background-color: #cce5ff; /* светло-синий */
+                font-size: 24px;
+                font-family: Arial, sans-serif;
+                padding: 20px;
+            }}
+            strong {{
+                color: #004085; /* тёмно-синий */
+            }}
+        </style>
+    </head>
+    <body>
+        <strong>Host IP:</strong> {ip}
+    </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
